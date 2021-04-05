3dae600ad3695d65e256caf8dda1c4d497300279
WesselvonderFuhr <38494974+WesselvonderFuhr@users.noreply.github.com>
2021-04-05 13:13:18 +0200

    tests

--- test/jailtest.js
+++ test/jailtest.js
@@ -1,60 +1,42 @@
 var request = require('supertest')
 var expect = require('chai').expect;
 var should = require('chai').should();
-
+var server = 'http://localhost:3000';
 var app = require('express')();
 var jail = require('../routes/jail');
 app.use('/', jail);
 
-function makeRequest(route, statusCode, done){
-	request('http://localhost:3000')
-		.get(route)
-		.expect(statusCode)
-		.end(function(err, res){
-			if(err){ return done(err); }
 
-			done(null, res);
-		});
-};
-
-describe('Testing calendar route', function(){
-	describe('without params', function(){
+describe('Testing jail route', function(){
+	describe('get', function(){
 		it('should return jails ', function(done){
-			makeRequest('/jail', 200, function(err, res){
-				if(err){ return done(err); }
-				expect(res.body).to.have.members;
-				let array = res.body;
-				array.forEach(element => {
-					expect(element).to.have.property('location');
-					expect(element.location).to.have.property('latitude');
-               		expect(element.location).to.have.property('longitude');
+			request(server)
+				.get('/jail')
+				.expect(200)
+				.end(function(err, res){
+					if(err){ return done(err); }
+					expect(res.body).to.have.members;
+					let array = res.body;
+					array.forEach(element => {
+						expect(element).to.have.property('location');
+						expect(element.location).to.have.property('latitude');
+						expect(element.location).to.have.property('longitude');
+					});
+					done();
 				});
+		});
+	});
+	describe('put jail', () => {
+		it('it should add a new jail', (done) => {
+			request(server)
+			.put('/jail')
+			.send({location:{latitude: 300,longitude:500}})
+			.expect(200)
+			.end(function(err, res){
+				res.statusCode.should.equal(200);
 				done();
 			});
 		});
 	});
-	
-	describe('put jail', () => {
-		it('it should add a new jail', (done) => {
-			var newLocation = {
-				location:
-				{
-					latitude: 300,
-					longitude: 500
-				}
-			}
-			let jail = new Jail({newLocation})
 
-			jail.save((err, jail) => {
-				  chai.request(server)
-				  .put('/jail')
-				  .send({location:{latitude: 300,longitude:500}})
-				  .end((err, res) => {
-						res.should.have.status(200);
-						res.body.should.have.property('message').eql('Update gelukt');
-					done();
-				  });
-			});
-		});
-	});
 });
