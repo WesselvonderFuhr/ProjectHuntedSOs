56dd2258d26c80801229ab66b0034760ea6e7f7e
WesselvonderFuhr <38494974+WesselvonderFuhr@users.noreply.github.com>
2021-04-15 17:39:50 +0200

    api game playfield

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ var loot = require('./Routes/loot.js');
 var player = require('./Routes/player.js');
 var jail = require('./Routes/jail.js');
 var accesscode = require('./Routes/accesscode.js');
+var game = require('./Routes/game.js');
 
 app.use(express.json())
 app.use('/loot', loot);
 app.use('/player', player);
 app.use('/jail', jail);
 app.use('/accesscode', accesscode);
+app.use('/game', game);
+
 
 app.listen(process.env.PORT || 3000);
