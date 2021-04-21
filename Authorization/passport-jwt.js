6e99a4d802ec9474506f404b28f88c93a9762e33
Schetsboek <jjmschet@avans.nl>
2021-04-21 14:37:42 +0200

    100% certified good

--- Authorization/passport-jwt.js
+++ Authorization/passport-jwt.js
@@ -2,36 +2,19 @@ const passport = require("passport");
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const secret = require('./secret');
+const UserPassport = require("./user-passport");
 
 let jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 jwtOptions.secretOrKey = secret;
 
-const mongoose = require('mongoose');
-const Administrator = mongoose.model('Administrator');
-const Accesscode = mongoose.model('Accesscode');
-
-let admin_strategy = new JwtStrategy(jwtOptions, async function(jwt_payload, next) {
-    let query = {code: jwt_payload.code};
-    let admin = await Administrator.findOne(query);
-    if (admin) {
-        next(null, admin);
+let strategy = new JwtStrategy(jwtOptions, async function(jwt_payload, next) {
+    if(jwt_payload.game_id != null && jwt_payload.role != null){
+        let user_passport = new UserPassport(jwt_payload.game_id, jwt_payload.player_id, jwt_payload.role);
+        next(null, user_passport);
     } else {
         next(null, false);
     }
 });
 
-let accesscode_strategy = new JwtStrategy(jwtOptions, async function(jwt_payload, next) {
-    let query = {code: jwt_payload.code};
-    let accesscode = await Accesscode.findOne(query);
-    if (accesscode) {
-        next(null, accesscode);
-    } else {
-        next(null, false);
-    }
-});
-
-//https://stackoverflow.com/questions/20052617/use-multiple-local-strategies-in-passportjs
-
-passport.use('jwt-admin', admin_strategy);
-passport.use('jwt-player', accesscode_strategy);
+passport.use('jwt', strategy);
