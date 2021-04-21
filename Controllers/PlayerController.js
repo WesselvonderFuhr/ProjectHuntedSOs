b458134640128763e452df1feca2349c5eed0c4a
WesselvonderFuhr <38494974+WesselvonderFuhr@users.noreply.github.com>
2021-04-21 16:39:32 +0200

    asdf

--- Controllers/PlayerController.js
+++ Controllers/PlayerController.js
@@ -3,20 +3,120 @@ const Result = require("../Helper/Result");
 
 let Player = require('../MongoDB/player');
 
+
 class PlayerController{
-    async getAllPlayers(){
-        return await Player.find().populate('loot');
+    async getAllPlayers(game_id){
+        let query = { _id: game_id };
+        return await Game.findOne(query).populate('player').players;
     }
 
     async getPlayerByID(id){
         let query = { _id: id };
-        return await Player.findOne(query).populate('loot');
+        let result = await Player.findOne(query).populate('loot');
+        if(result == null){
+            return new Result(404, "Player does not exist");
+        }
+        return result;
     }
 
-    async addPlayer(body){
+    async getArrestablePlayers(id,distance){
+        if(id.length < 25){
+            if(!parseInt(distance)){
+                return new Result(400).send("Could not parse int distance");
+            }
+            //return list of id's that are close within distance
+            var playerLoc
+            Player.find({}, function (err, result) {
+                if (!err) {
+                    var distances = [];
+                    result.map(function (player) {
+                        if (id == player.id) {
+                            playerLoc = player.location
+                        }
+                    })
+                    if (playerLoc == null){
+                        return new Result(404, "Player does not exist");
+                    } else{
+                        if (playerLoc.latitude == null) {
+                            return new Result(400, "Player has no valid distance");
+                        }
+                        result.forEach(item => {
+                            if (item.id != id) {
+                                if (item.location.latitude != null) {
+                                    if (item.arrested == false && item.role == "Boef"){
+                                        var s = geolib.getPreciseDistance(
+                                            { latitude: playerLoc.latitude, longitude: playerLoc.longitude },
+                                            { latitude: item.location.latitude, longitude: item.location.longitude }
+                                        );
+                                        if (s <= distance){
+                                            distances.push({
+                                                'id': item.id
+                                            });
+                                        }
+                                    }
+                                }
+                            }
+                        })
+                    }
+                    return new Result(200, JSON.stringify(distances, null, 2));
+                }
+                else{
+                    return null;
+                }
+            });
+        }else{
+            return new Result(404, "Id does not exist");
+        }
+    }
+
+    async CheckPlayerOutOfBounds(playerID){
+        var playerQuery = {_id: playerID}
+        var indexFound = null
+        try{
+            var game = await Game.find({}).exec()
+            var player = await Player.findOne(playerQuery).exec()
+    
+            for(var i = 0; i < game.length; i++){
+                for(var x = 0; x < game[i].players.length; x++){
+                    if(game[i].players[x] == playerID){
+                        indexFound = i
+                        break;
+                    }
+                }
+            }
+    
+            var polyLocations = []
+            game[indexFound].playfield.forEach(loc =>
+                polyLocations.push({latitude: loc.location.latitude, longitude: loc.location.longitude})
+            )
+    
+            if(indexFound != null){
+                if(player.location.latitude != null){
+                    var isPoint = geolib.isPointInPolygon(player.location, polyLocations)
+                    
+                    res.status(200).send(!isPoint)
+                }else{
+                    return new Result(400, "Player does not have a location");
+                }
+            }else{
+                return new Result(400, "Player does not exist in any game");
+            }
+        }catch(e){
+            return new Result(400, e);
+        }
+    }
+
+    async addPlayer(game_id,body){
         let player = new Player(body);
         try {
+            let query = { _id: game_id };
+            let game = await Game.findOne(query);
+            if(game == null){
+                return new Result(404, "Game does not exist");
+            }
+            game.players.push(player._id);
             await player.save();
+            await game.save();
             return new Result(200, player.name + " has been added");
         }
         catch (e){
@@ -37,24 +137,7 @@ class PlayerController{
         }
         catch{
             return null;
-        }
-        
-    }
-
-    async deletePlayer(id){
-        let query = { _id: id };
-        try{
-            let player = await this.getPlayerByID(id);
-            if(player != null){
-                await Player.deleteOne(query);
-                return new Result(200, player.name + " has been deleted");
-            } else {
-                return new Result(404, "Player not found");
-            }
-        }
-        catch{
-            return null;
-        }
+        }   
     }
 
 
--- Routes/player.js
+++ Routes/player.js
@@ -4,49 +4,18 @@ const Loot = require('../MongoDB/loot');
 const Game = require('../MongoDB/game');
 const router = express.Router();
 const geolib = require('geolib');
-
-router.post('/:codeId/:username', async function (req, res) {
-    var emptyLoc = { latitude: null, longitude: null }
-    var name = req.params.username;
-    var codeId = req.params.codeId
-    var accessCode = await Accesscode.findOne({code: codeId})
-
-    let player = {};
-    player.name = name;
-    player.role = accessCode.role;
-    player.arrested = false;
-    player.location = emptyLoc;
-
-    let playerModel = new Player(player);
-    await playerModel.save();
-
-    console.log(playerModel)
-
-    accessCode.assignedTo = playerModel._id
-
-    await accessCode.save()
-
-    res.status(200).send(accessCode);
-});
-
+let PlayerController = require('../Controllers/PlayerController');
+const {DefaultResponse} = require("../Helper/DefaultResponse");
+//get
 router.get('/', function (req, res) {
-    var players = Player.find({}, function (err, result) {
-        if (!err) {
-            res.send(result);
-        }
-    });
+    let result = await PlayerController.getAllPlayers(req.query.game_id);
+    return res.status(result.responseCode).json(result.message);
 });
 
 
 router.get('/:id', function (req, res) {
-    var query = { _id: req.params.id };
-    Player.findOne(query, function (err, result) {
-        if (!err) {
-            res.status(200).send(result);
-        }else{
-            res.status(404).send("User does not exist")
-        }
-    });
+    let result =  await PlayerController.getPlayerByID(req.params.id);
+    return res.status(result.responseCode).json(result.message);
 });
 
 router.get('check/:name', async function (req, res) {
@@ -60,118 +29,19 @@ router.get('check/:name', async function (req, res) {
                 }
             }
         }
-
         res.status(200).send('Not found');
     });
 });
 
 router.get('/arrestableThieves/:id/:distance', function(req, res){
-    if(req.params.id.length < 25){
-        if(!parseInt(req.params.distance)){
-            res.status(400).send("Could not parse int distance")
-            return
-        }
-        //return list of id's that are close within distance
-        var playerLoc
-        var players = Player.find({}, function (err, result) {
-            if (!err) {
-                var distances = [];
-                result.map(function (player) {
-                    if (req.params.id == player.id) {
-                        playerLoc = player.location
-                    }
-                })
-                if (playerLoc == null){
-                    res.status(404).send("User does not exist")
-                    return
-                } else{
-                    if (playerLoc.latitude == null) {
-                        res.status(400).send("Player has no valid distance")
-                        return
-                    }
-                    result.forEach(item => {
-                        if (item.id != req.params.id) {
-                            if (item.location.latitude != null) {
-                                if (item.arrested == false && item.role == "Boef"){
-                                    var s = geolib.getPreciseDistance(
-                                        { latitude: playerLoc.latitude, longitude: playerLoc.longitude },
-                                        { latitude: item.location.latitude, longitude: item.location.longitude }
-                                    );
-                                    if (s <= req.params.distance){
-                                        distances.push({
-                                            'id': item.id
-                                        });
-                                    }
-                                }
-                            }
-                        }
-                    })
-                }
-                
-                function finished(err) {
-                    console.log(err)
-                }
-                res.send(JSON.stringify(distances, null, 2))
-            }
-            else{
-                res.send(error);
-            }
-        });
-    }else{
-        res.sendStatus(404);
-    }
-    
+    let result =  await PlayerController.getArrestablePlayers(req.params.id,req.params.distance); 
+    return res.status(result.responseCode).json(result.message);
 });
 
 router.get('/outofbounds/:id', async function(req, res){
-    var playerQuery = {_id: req.params.id}
-    var indexFound = null
-    try{
-        var game = await Game.find({}).exec()
-        var player = await Player.findOne(playerQuery).exec()
-
-        for(var i = 0; i < game.length; i++){
-            for(var x = 0; x < game[i].players.length; x++){
-                if(game[i].players[x] == req.params.id){
-                    indexFound = i
-                    break;
-                }
-            }
-        }
-
-        var polyLocations = []
-        game[indexFound].playfield.forEach(loc =>
-            polyLocations.push({latitude: loc.location.latitude, longitude: loc.location.longitude})
-        )
-
-        if(indexFound != null){
-            if(player.location.latitude != null){
-                var isPoint = geolib.isPointInPolygon(player.location, polyLocations)
-                res.status(200).send(!isPoint)
-            }else{
-                res.status(400).send('Player does not have a location')
-            }
-        }else{
-            res.status(400).send('Player does not exist in any game')
-        }
-    }catch(e){
-        res.status(400).send(e)
-    }
+    let result = await PlayerController.CheckPlayerOutOfBounds(req.params.id); 
+    return res.status(result.responseCode).json(result.message);
 });
-
-router.put('/arrest/:thiefId', async (req, res) => {
-    var query = { _id: req.params.thiefId };
-    var arrestQuery = {arrested: true,
-    loot: []}
-
-    Player.updateOne(query, arrestQuery, function (err, result) {
-        function finished(err) {
-            console.log(err)
-        }
-        res.send("Boef gevangen!");
-    })
-
-})
 
 router.get('/distances/:id', function (req, res) {
     var playerLoc
@@ -211,21 +81,29 @@ router.get('/distances/:id', function (req, res) {
     });
 });
 
-router.put('/location/:id', function (req, res) {
-    var query = { _id: req.params.id };
-    var newLoc = {
-        location:
-        {
-            latitude: req.body.location.latitude,
-            longitude: req.body.location.longitude
-        }
-    }
-    Player.updateOne(query, newLoc, function (err, result) {
-        function finished(err) {
-            console.log(err)
-        }
-        res.send("Update gelukt");
-    })
+//post
+router.post('/:codeId/:username', async function (req, res) {
+    var emptyLoc = { latitude: null, longitude: null }
+    var name = req.params.username;
+    var codeId = req.params.codeId
+    var accessCode = await Accesscode.findOne({code: codeId})
+
+    let player = {};
+    player.name = name;
+    player.role = accessCode.role;
+    player.arrested = false;
+    player.location = emptyLoc;
+
+    let playerModel = new Player(player);
+    await playerModel.save();
+
+    console.log(playerModel)
+
+    accessCode.assignedTo = playerModel._id
+
+    await accessCode.save()
+
+    res.status(200).send(accessCode);
 });
 
 router.post('/:playerid/stolen/:lootid', function (req, res) {
@@ -260,5 +138,43 @@ router.post('/:playerid/stolen/:lootid', function (req, res) {
         }      
     });
 });
+//put
+router.put('/arrest/:thiefId', async (req, res) => {
+    var query = { _id: req.params.thiefId };
+    var arrestQuery = {arrested: true,
+    loot: []}
+
+    Player.updateOne(query, arrestQuery, function (err, result) {
+        function finished(err) {
+            console.log(err)
+        }
+        res.send("Boef gevangen!");
+    })
+
+});
+router.put('/location/:id', function (req, res) {
+    var query = { _id: req.params.id };
+    var newLoc = {
+        location:
+        {
+            latitude: req.body.location.latitude,
+            longitude: req.body.location.longitude
+        }
+    }
+    Player.updateOne(query, newLoc, function (err, result) {
+        function finished(err) {
+            console.log(err)
+        }
+        res.send("Update gelukt");
+    })
+});
+
+
+
+
+
+
+
+
 
 module.exports = router;
