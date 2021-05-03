d5fb2bf636a45ae63c987d90b9ac8270c78938df
sjors37 <sjorshekerman@LIVE.NL>
2021-05-03 11:28:49 +0200

    gekke bugfix

--- Routes/player.js
+++ Routes/player.js
@@ -17,6 +17,15 @@ router.get('/', passport.authenticate('jwt', { session: false }), async function
     return res.status(result.responseCode).json(result.message);
 });
 
+router.get('/outofbounds', passport.authenticate('jwt', { session: false }), async function(req, res){
+        let result = await PlayerController.CheckPlayerOutOfBounds(req.user.player_id);
+        return res.status(result.responseCode).json(result.message);
+});
+
+router.get('/distances', passport.authenticate('jwt', { session: false }), async function (req, res) {
+    let result = await PlayerController.GetPlayerDistances(req.user.player_id, req.user.game_id);
+    return res.status(result.responseCode).json(result.message);
+});
 
 router.get('/:id', passport.authenticate('jwt', { session: false }), async function (req, res) {
     let result =  await PlayerController.getPlayerByID(req.params.id);
@@ -31,22 +40,11 @@ router.get('/check/:id', passport.authenticate('jwt', { session: false }), async
 
 router.get('/arrestableThieves/:distance', passport.authenticate('jwt', { session: false }), async function(req, res){
     //TODO Only agent
-    let result =  await PlayerController.getArrestablePlayers(req.user.player_id,req.user.game_id,req.params.distance);
+    let result =  await PlayerController.getArrestablePlayers(req.user.player_id,req.params.distance);
     console.log(result)
     return res.status(result.responseCode).json(result.message);
 });
 
-router.get('/outofbounds', passport.authenticate('jwt', { session: false }), async function(req, res){
-    //TODO help
-    let result = await PlayerController.CheckPlayerOutOfBounds(req.user.player_id,req.user.game_id);
-    return res.status(result.responseCode).json(result.message);
-});
-
-router.get('/distances', passport.authenticate('jwt', { session: false }), async function (req, res) {
-    //TODO help
-    let result = await PlayerController.GetPlayerDistances(req.user.player_id);
-    return res.status(result.responseCode).json(result.message);
-});
 
 router.post('/stolen/:lootid', passport.authenticate('jwt', { session: false }), async function (req, res) {
     let result = await PlayerController.StealLoot(req.user.player_id, req.params.lootid);
@@ -54,7 +52,7 @@ router.post('/stolen/:lootid', passport.authenticate('jwt', { session: false }),
 });
 
 //put
-router.put('/arrest/:thiefId', passport.authenticate('jwt', { session: false }), async function (req, res) {
+router.put('/arrest/:thiefId', passport.authenticate('jwt', { session: false }), async (req, res) => {
     var arrestQuery = { arrested: true, loot: [] }
     let result = await PlayerController.editPlayer(req.params.thiefId, arrestQuery); 
     return res.status(result.responseCode).json(result.message);
