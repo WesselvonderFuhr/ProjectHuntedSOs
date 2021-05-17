dfbeaa107de2b0685340df5b7fc3cf6e7508fa98
sjors37 <sjorshekerman@LIVE.NL>
2021-05-17 19:01:19 +0200

    mappie

--- app/src/main/java/com/example/hunted/thieves/ThievesFragmentLocations.java
+++ app/src/main/java/com/example/hunted/thieves/ThievesFragmentLocations.java
@@ -1,5 +1,7 @@
 package com.example.hunted.thieves;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +13,20 @@ import androidx.fragment.app.Fragment;
 
 import com.example.hunted.R;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Polygon;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThievesFragmentLocations extends Fragment {
     private TextView tvIsArrested;
+    private MapView map = null;
+
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +45,53 @@ public class ThievesFragmentLocations extends Fragment {
         if(requireArguments().getBoolean("isArrested")){
             isArrested();
         }
+
+        Context ctx = this.getActivity().getApplicationContext();
+        Configuration.getInstance().setUserAgentValue(ctx.getPackageName());
+
+        map = getView().findViewById(R.id.mapView);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.getController().setZoom(19.0);
+
+        map.getController().setCenter(new GeoPoint(51.6890463, 5.3035104));
+
+        ArrayList points = new ArrayList<>();
+        points.add(new GeoPoint(51.6896881, 5.3021801));
+        points.add(new GeoPoint(51.6883945, 5.3026468));
+        points.add(new GeoPoint(51.6889864, 5.3051680));
+        points.add(new GeoPoint(51.6899142, 5.3043956));
+        points.add(new GeoPoint(51.6896881, 5.3021801));
+
+        List<List<GeoPoint>> holes = new ArrayList<>();
+
+        List<GeoPoint> h1 = new ArrayList<GeoPoint>();
+        h1.add(new GeoPoint( 51.6892059, 5.3037223));
+        h1.add(new GeoPoint( 51.6891909, 5.3033575));
+        h1.add(new GeoPoint( 51.6889665, 5.3033468));
+        h1.add(new GeoPoint( 51.6889465, 5.3037760));
+        h1.add(new GeoPoint( 51.6892059, 5.3037223));
+        holes.add(h1);
+
+        Polygon p = new Polygon();
+        p.setPoints(points);
+        p.setHoles(holes);
+
+        Polygon p2 = new Polygon();
+        points.clear();
+        points.add(new GeoPoint(51.6880087, 5.3001952));
+        points.add(new GeoPoint(51.6868979, 5.2994978));
+        points.add(new GeoPoint(51.6866917, 5.3022873));
+        points.add(new GeoPoint(51.6877559, 5.3025234));
+        points.add(new GeoPoint(51.6880087, 5.3001952));
+        p2.setPoints(points);
+
+        p.getFillPaint().setColor(Color.BLUE);
+        p.getFillPaint().setAlpha(20);
+        p2.getFillPaint().setColor(Color.RED);
+        p2.getFillPaint().setAlpha(20);
+
+        map.getOverlays().add(p);
+        map.getOverlays().add(p2);
     }
 
     public void isArrested(){
