32f3fcd8d9eaf727456d69f9cf426a207aa9aa06
WesselvonderFuhr <38494974+WesselvonderFuhr@users.noreply.github.com>
2021-05-21 16:10:12 +0200

    yes

--- test/PlayerTest.js
+++ test/PlayerTest.js
@@ -126,7 +126,7 @@ describe('player CheckPlayersOutOfBounds ', async function(){
 });
 //CheckPlayerOutOfBounds
 /*
-describe('player CheckPlayerOutOfBounds ', async function(){
+describe('player  CheckPlayerOutOfBounds ', async function(){
     it('get CheckPlayerOutOfBounds  200', async ()  => {
         let player = await Player.findOne();
         player.location = {
