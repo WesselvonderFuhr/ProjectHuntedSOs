d14d7fff282c9e30cd59fc8fcb667a432294f934
sjors37 <sjorshekerman@LIVE.NL>
2021-05-25 11:47:14 +0200

    bugfix

--- app/src/main/java/com/example/hunted/thieves/ThievesFragmentLocations.java
+++ app/src/main/java/com/example/hunted/thieves/ThievesFragmentLocations.java
@@ -113,7 +113,8 @@ public class ThievesFragmentLocations extends Fragment {
                         double locLong = holesArea.getJSONObject(holeLoc).getDouble("longitude");
                         holes.add(new GeoPoint(locLat, locLong));
                     }
-                    holesList.add(holes);
+                    holesList.add(new ArrayList<GeoPoint>(holes));
+                    holes.clear();
                 }
                 playfieldPolygon.setPoints(playField);
                 playfieldPolygon.setHoles(holesList);
