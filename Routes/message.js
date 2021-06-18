49ead1e87851186468c5f71b8be2ae58bbb03e45
Luuk Pook <luukpook@gmail.com>
2021-06-18 13:07:15 +0200

    Removed auth on message routes

--- Routes/message.js
+++ Routes/message.js
@@ -7,23 +7,14 @@ const authorize = require("../Authorization/authorize");
 const {ResponseHandler} = require("../Helper/ResponseHandler");
 
 router.post('/', passport.authenticate('jwt', { session: false }), async function (req, res) {
-    let unauthorized = await authorize.Administrator(req.user);
-    if(unauthorized){
-      return ResponseHandler(unauthorized, req, res);
-    }
-  
-    let result = await MessageController.addMessage(req.user.game_id, req.body);
-    ResponseHandler(result, req, res);
-  });
+  let result = await MessageController.addMessage(req.user.game_id, req.body);
+  ResponseHandler(result, req, res);
+});
 
-  router.get('/', passport.authenticate('jwt', { session: false }), async function (req, res) {
-    let unauthorized = await authorize.Player(req.user);
-    if(unauthorized){
-      return ResponseHandler(unauthorized, req, res);
-    }
-    let result = await MessageController.getAllMessages(req.user.game_id);
-    ResponseHandler(result, req, res);
-  });
+router.get('/', passport.authenticate('jwt', { session: false }), async function (req, res) {
+  let result = await MessageController.getAllMessages(req.user.game_id);
+  ResponseHandler(result, req, res);
+});
 
 
 module.exports = router;
